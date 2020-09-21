ebd681d1a859b97523fe18aea2b14a9a06c31d2d
bsrinath4839 <bsrinath4839@gmail.com>
2020-09-21 12:30:37 +0530

    commit

--- config.js
+++ config.js
@@ -4,7 +4,4 @@ dotenv.config();
 module.exports = {
     dburl: process.env.DB_CONNECT_URL,
     port: process.env.PORT,
-    secret: process.env.SECRET,
-    mymail: process.env.MY_MAIL,
-    mymailpassword: process.env.MY_MAIL_PASSWORD,
 };
